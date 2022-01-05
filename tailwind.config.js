3bfd119b74fb0ae8098c36af8be51ffc870dba84
German Solano <kontrolg1239@gmail.com>
2022-01-05 08:04:18 -0400

    feat(TailwindCSS): :lipstick: Add theme fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Barlow Condensed", "sans-serif"],
+      serif: ["Bellefair", "serif"]
+    },
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
