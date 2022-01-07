1dde4af1f8381728b514d6e70bb68e4ff55a24f7
German Solano <kontrolg1239@gmail.com>
2022-01-07 14:25:15 -0400

    feat(<Header />): :lipstick: Add styles for desktop

--- components/layouts/header/index.jsx
+++ components/layouts/header/index.jsx
@@ -4,7 +4,7 @@ import { Navigation } from "../navigation";
 
 function Header() {
   return (
-    <div className="container pt-6 flex justify-between z-10">
+    <div className="container pt-6 lg:pt-10 flex justify-between items-center z-10">
       <Link href={HOME_PATH}>
         <img
           src={SHARED_IMAGES_PATH + "/logo.svg"}
@@ -12,6 +12,7 @@ function Header() {
           className="w-10 h-10 md:w-12 md:h-12"
         />
       </Link>
+      <hr className="hidden xl:block absolute border-white ml-28 2xl:ml-48 border-opacity-25 w-[32.8472222vw] z-20" />
       <Navigation />
     </div>
   );
--- components/layouts/navigation/index.jsx
+++ components/layouts/navigation/index.jsx
@@ -13,7 +13,7 @@ function Navigation() {
 
   return (
     <>
-      <button className="p-2 z-20" onClick={toggleMenu}>
+      <button className="p-2 z-20 lg:hidden" onClick={toggleMenu}>
         <img
           src={`${SHARED_IMAGES_PATH}/${
             isOpenMenu ? "icon-close.svg" : "icon-hamburger.svg"
@@ -23,24 +23,29 @@ function Navigation() {
       </button>
       <div
         className={classes(
-          "bg-white bg-opacity-5 backdrop-blur-xl h-screen w-[254px] transition-transform duration-500 fixed top-0 right-0 z-10",
+          "bg-white bg-opacity-5 backdrop-blur-xl h-screen lg:h-auto w-[254px] lg:w-auto transition-transform duration-500 fixed top-0 lg:top-auto right-0 z-10 lg:translate-x-0 lg:pl-20 xl:pl-32 lg:pr-28 xl:pr-40",
           !isOpenMenu && "translate-x-full"
         )}
       >
-        <nav className="mt-28 space-y-4" id="main-navigation">
+        <nav
+          className="mt-28 lg:mt-0 lg:flex space-y-4 lg:space-y-0 lg:space-x-12"
+          id="main-navigation"
+        >
           {navigationItems.map((item, index) => (
             <NavLink
               exact={item.exact}
               key={item.id}
               to={item.to}
-              className="uppercase font-subheading tracking-[2.7px] block pl-8 py-1  border-r-4 border-transparent hover:border-white hover:border-opacity-50 transition-colors"
-              activeClassName="border-r-4 border-white hover:border-opacity-100"
+              className="uppercase font-subheading tracking-[2.7px] block pl-8 lg:pl-0 py-1 lg:py-7 xl:py-9 border-r-4 lg:border-r-0 lg:border-b-4 border-transparent hover:border-white hover:border-opacity-50 transition-colors"
+              activeClassName="border-white hover:border-opacity-100"
             >
               <span className="font-bold">0{index}</span> {item.label}
             </NavLink>
           ))}
         </nav>
       </div>
+      {/* To simulate Navigation height to align properly with other items in the flex container. */}
+      <div className="hidden lg:block lg:h-[5.25rem] xl:h-[6.25rem]" />
     </>
   );
 }
